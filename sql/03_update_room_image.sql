110c9e27f127dea62c29daf0614e934a129943f1
bgawlik9 <47053060+bgawlik9@users.noreply.github.com>
2021-07-11 20:05:11 +0200

    dodanie do bazy zdjec sal

--- sql/03_update_room_image.sql
+++ sql/03_update_room_image.sql
@@ -6,3 +6,9 @@ UPDATE `sale` SET `obrazUrl` = 'images/room2.png' WHERE `id`=2;
 UPDATE `sale` SET `obrazUrl` = 'images/room3.png' WHERE `id`=3;
 UPDATE `sale` SET `obrazUrl` = 'images/room4.png' WHERE `id`=4;
 UPDATE `sale` SET `obrazUrl` = 'images/room5.png' WHERE `id`=5;
+
+UPDATE `sale` SET `obrazUrl` = 'images/room12.png' WHERE `id`=1;
+UPDATE `sale` SET `obrazUrl` = 'images/room165.png' WHERE `id`=2;
+UPDATE `sale` SET `obrazUrl` = 'images/room35.png' WHERE `id`=3;
+UPDATE `sale` SET `obrazUrl` = 'images/room90.png' WHERE `id`=4;
+UPDATE `sale` SET `obrazUrl` = 'images/room45.png' WHERE `id`=5;
